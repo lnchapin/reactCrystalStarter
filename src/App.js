f75426d723444306601da7d203b61a30f43d6dda
Lindsay Chapin <lnchapin@gmail.com>
2019-08-28 17:19:06 -0600

    reset

--- src/App.js
+++ src/App.js
@@ -1,81 +1,31 @@
 import React, { Component } from "react";
-import CrystalPics from "./components/CrystalPictures";
-import Scores from "./components/Scores";
-import Header from "./components/Header";
-import crystals from "./crystals.json";
-import Data from "./components/HistoricData"
-import Wrapper from "./components/Wrapper"
+// import CrystalPics from "./components/CrystalPictures";
+// import Scores from "./components/Scores";
+// import Header from "./components/Header"
+// import crystals from "./crystals.json";
+// import Data from "./components/HistoricData"
+// import Wrapper from "./components/Wrapper"
 import './App.css';
-
-
 class App extends Component {
-  state={
-    crystals,
-    score: 0,
-    value: [0, 0, 0, 0],
-    target: 0,
-    wins: 0,
-    loses:0
-  }
-
-  componentDidMount=()=>{
-    this.updateValue()
-    this.updateTarget()
-  }
-
-  handleScore = (id, value) =>{
-    console.log(`in handleScore ${id} ${value}`);
-    this.setState({ score: this.state.score + value})
-    if (this.state.score === this.state.target) {
-      console.log("You Win");
-      this.updateValue()
-      this.updateTarget()
-      let newWins = this.state.wins + 1
-      this.setState({ score: 0, wins: newWins})
-    } else if (this.state.score > this.state.target) {
-      console.log("you Lose");
-      this.updateValue()
-      this.updateTarget()
-      let newLoses = this.state.loses + 1
-      this.setState({ score: 0, loses: newLoses})
-    }
-  }
-
-
-  updateValue =()=>{
-    let newValues =this.state.value.map(x=>(x=(Math.floor(Math.random() * 12)+1)))
-    this.setState({ value : newValues})
-  }
-
-  updateTarget =()=>{
-    let newTarget = (Math.floor(Math.random() * (120 -19 + 1)))
-    this.setState({ target : newTarget})
-  }
-
   render() {
     return (
       <div className="App">
-        <Header />
-        <Scores targetScore={this.state.target} score={this.state.score}/>
-        <Wrapper>
-          {
-            this.state.crystals.map(crystal =>
-              <CrystalPics
-              className="image"
-              name={crystal.name}
-              image={crystal.picture}
-              id={crystal.id}
-              key={crystal.id}
-              value={this.state.value[crystal.id]}
-              score={this.handleScore}
-              />
-            )
-          }
-        </Wrapper>
-        <Data wins={this.state.wins} loses={this.state.loses} />
+        <header className="App-header">
+          <h1>We're going to turn this in to Crystal Collector</h1>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
 }
-
 export default App;
